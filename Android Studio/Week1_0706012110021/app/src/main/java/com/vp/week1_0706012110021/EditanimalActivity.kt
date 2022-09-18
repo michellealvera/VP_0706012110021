82e767bb87933914493084966312bc8bca0a2f80
michellealvera <malveralolang@student.ciputra.ac.id>
2022-09-18 23:03:08 +0700

    Edit n Delete

---
+++ Android Studio/Week1_0706012110021/app/src/main/java/com/vp/week1_0706012110021/EditanimalActivity.kt
@@ -0,0 +1,10 @@
+package com.vp.week1_0706012110021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditanimalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
