bcff4ac6f9d475f83efa2cd51dcf8bb39d69a78a
michellealvera <89299208+michellealvera@users.noreply.github.com>
2022-10-16 21:56:50 +0700

    Add files via upload

---
+++ Week5/app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_vp_week4retrofit_Application.java
@@ -0,0 +1,19 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.vp.week4retrofit.Application",
+    rootPackage = "com.vp.week4retrofit",
+    originatingRoot = "com.vp.week4retrofit.Application",
+    originatingRootPackage = "com.vp.week4retrofit",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "Application",
+    originatingRootSimpleNames = "Application"
+)
+public class _com_vp_week4retrofit_Application {
+}
